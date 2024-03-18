5d60cd0549a3b4ffdbf4d9dbaac6a2fb9e26b052
Somraj <somrajbishnoirock29@gmail.com>
2024-03-18 23:07:04 +0530

    Update loading.js

--- app/loading.js
+++ app/loading.js
@@ -1,10 +1,10 @@
 const Loading = () => {
   return (
-    <div class="relative flex justify-center items-center pt-[15rem]">
-      <div class="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-purple-500"></div>
+    <div className="relative flex justify-center items-center pt-[15rem]">
+      <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-purple-500"></div>
       <img
         src="https://www.svgrepo.com/show/509001/avatar-thinking-9.svg"
-        class="rounded-full h-28 w-28 "
+        className="rounded-full h-28 w-28 "
       />
     </div>
   );
